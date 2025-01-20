27b6448982dc6b2e4a862f6b5c117f0967f865e0
sehamanter1 <sehamanter2@gmail.com>
2025-01-20 21:18:02 +0200

    #initial - custom user

---
+++ accounts/admin.py
@@ -0,0 +1,33 @@
+# accounts/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from django.utils.translation import gettext_lazy as _
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')   
+    ordering = ('email',)
+
+    # user creation and change form
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+    # Save method for automatically setting the username to email
+    def save_model(self, request, obj, form, change):
+        if not obj.username:
+            obj.username = obj.email 
+        super().save_model(request, obj, form, change)
