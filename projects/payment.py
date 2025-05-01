c1e107396483bdd30164403d9d5089d684823eb5
sehamanter1 <sehamanter2@gmail.com>
2025-05-02 00:56:10 +0300

    get price and product data from request

--- projects/payment.py
+++ projects/payment.py
@@ -3,13 +3,18 @@ from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-
+from .models import Project
 stripe.api_key = settings.STRIPE_SECRET_KEY
 print('api_key',settings.STRIPE_SECRET_KEY)
 class CreateCheckoutSessionView(APIView):
     def post(self, request):
         try:
             YOUR_DOMAIN = "http://localhost:3000"  
+            project_id = request.data.get('projectId')
+            price = request.data.get('price')
+            project = Project.objects.get(id=project_id)
+            project_name = project.title
+            project_image_url = project.cover.url
             checkout_session = stripe.checkout.Session.create(
                 payment_method_types=['card'],
                 line_items=[
@@ -17,10 +22,10 @@ class CreateCheckoutSessionView(APIView):
                         'price_data': {
                             'currency': 'usd',
                             'product_data': {
-                                'name': 'T-shirt',
-                                'images':['https://res.cloudinary.com/dovnjzpgf/image/upload/v1/media/project_covers/advantimg_ajtbvh'],
+                                'name': project_name,
+                                'images':[project_image_url],
                             },
-                            'unit_amount': 2000, 
+                            'unit_amount': price*100, 
                         },
                         'quantity': 1,
                     },
