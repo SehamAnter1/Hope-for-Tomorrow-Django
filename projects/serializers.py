f67219550e271cb8b93212f5dd6855544b285ffd
sehamanter1 <sehamanter2@gmail.com>
2025-05-03 04:07:48 +0300

    retrund top donors in project details

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Donation, Project,Category
 from rest_framework.exceptions import ValidationError
+from django.db.models import Sum,F
 
 
 #_______________ CategorySerializer ________________
@@ -14,6 +15,7 @@ class CategorySerializer(serializers.ModelSerializer):
 class ProjectSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.email')
     categories = serializers.SerializerMethodField()
+    top_donors  = serializers.SerializerMethodField()
 
     class Meta:
         model = Project
@@ -22,6 +24,22 @@ class ProjectSerializer(serializers.ModelSerializer):
 
     def get_categories(self, obj):
         return [category.title for category in obj.categories.all()]
+    def get_top_donors(self, obj):
+        top_donors = (
+            Donation.objects
+            .filter(project=obj)
+            .values('user_id', username=F('user__email'))
+            .annotate(total_donated=Sum('amount'))
+            .order_by('-total_donated')[:5]
+        )
+        return TopDonatorsSerializer(top_donors, many=True).data
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        request = self.context.get('request')
+        if not request or not request.parser_context.get('kwargs', {}).get('pk'):
+            data.pop('top_donors', None)
+        return data
 
 #_______________ DonationSerializer ________________
 
