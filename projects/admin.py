324539d764257dab7c2498c283e668c1fa8e37e5
sehamanter1 <sehamanter2@gmail.com>
2025-06-27 12:47:13 +0300

    add inline images in admin in project

--- projects/admin.py
+++ projects/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
 from .models import Project, Category, Donation, ProjectImage
+class ProjectImageInline(admin.TabularInline):  # Or use admin.StackedInline
+    model = ProjectImage
+    extra = 1 # to add each time
+    fields = ['image']  # only show image field in inline form
+    show_change_link = False
+    can_delete = True
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'price_goal', 'donations_amount', 'progress', 'user', 'created_at')
     search_fields = ('title', 'description')
     list_filter = ('categories', 'created_at')
-
+    inlines = [ProjectImageInline]
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'title_ar')
@@ -18,3 +24,4 @@ class DonationAdmin(admin.ModelAdmin):
 @admin.register(ProjectImage)
 class ProjectImageAdmin(admin.ModelAdmin):
     list_display = ('project', 'image')
+    
